91d8cd337e46e6f428348b6124246bd15a59ead2
Creed <creedonhood@gmail.com>
2025-04-22 02:15:22 -0400

    check for predictedHealth cvar

--- Core.lua
+++ Core.lua
@@ -3,6 +3,13 @@ local AceAddon = LibStub("AceAddon-3.0")
 
 OvershieldsReforged = AceAddon:NewAddon(ADDON_NAME, "AceEvent-3.0", "AceConsole-3.0")
 
+local function CheckIncomingHealsOption()
+	if not GetCVarBool("predictedHealth") then
+		print(
+			"|cffff0000[Overshields Reforged]|r Warning: The 'Display Incoming Heals' option is disabled. This addon requires it to function properly. Please enable it in Game Menu > Options > Interface > Raid Frames > Display Incoming Heals. It is recommended to disable this addon if you do not want to see extra absorb shields.")
+	end
+end
+
 function OvershieldsReforged:OnInitialize()
 	-- Initialize database and options
 	self:InitializeDatabase()
@@ -15,6 +22,19 @@ function OvershieldsReforged:OnInitialize()
 	hooksecurefunc("CompactUnitFrame_UpdateHealPrediction", function(frame)
 		ns.HandleCompactUnitFrame_Update(frame)
 	end)
+end
+
+function OvershieldsReforged:OnEnable()
+	-- Periodically check the "Display Incoming Heals" option
+	self.incomingHealsCheckTicker = C_Timer.NewTicker(30, CheckIncomingHealsOption)
+end
+
+function OvershieldsReforged:OnDisable()
+	-- Cancel the periodic check when the addon is disabled
+	if self.incomingHealsCheckTicker then
+		self.incomingHealsCheckTicker:Cancel()
+		self.incomingHealsCheckTicker = nil
+	end
 end
 
 -- Slash command handler
