d1a6135c69f26a2355f5669302b72b22d7616d22
Creed <creedonhood@gmail.com>
2025-06-11 17:04:16 -0400

    call ClearAllPoints thru local func instead of hooking UpdateFillBar

--- CompactUnitFrame_Update.lua
+++ CompactUnitFrame_Update.lua
@@ -1,19 +1,19 @@
 local _, ns = ...
 local OVERSHIELD_TICK_OFFSET = -7
 
--- Hook into the base UI function to clear points before it sets them
-hooksecurefunc("CompactUnitFrameUtil_UpdateFillBar", function(frame, previousTexture, bar, amount, barOffsetXPercent)
-	if type(bar) == "userdata"
-		and bar.ClearAllPoints
-		and not (bar.IsForbidden and bar:IsForbidden())
-	then
-		-- TODO: Handle error - determine if needed?
-		-- (discarded) success, error
-		local _, _ = pcall(function()
+local function clearAllPoints(bar)
+	-- TODO: Catch error - determine if handling needed?
+	-- _, (discarded) error
+	local success, _ = pcall(function()
+		if bar.ClearAllPoints and not (bar.IsForbidden and bar:IsForbidden()) then
 			bar:ClearAllPoints()
-		end)
+		end
+	end)
+	if not success then
+		print("[OSR] CompactUnitFrameUtil_UpdateFillBar: ClearAllPoints produced an error for: ", bar:GetName())
+		print("[OSR] Please report this! (https://github.com/creed-us/Overshields_Reforged).")
 	end
-end)
+end
 
 ns.HandleCompactUnitFrame_Update = function(frame)
 	local db = OvershieldsReforged.db.profile
@@ -62,7 +62,8 @@ ns.HandleCompactUnitFrame_Update = function(frame)
 
 	-- Handle overshieldTick prior to shieldOverlay and shieldBar to ensure correct visibility
 	if hasOvershield then
-		overshieldTick:ClearAllPoints()
+		clearAllPoints(overshieldTick)
+		--overshieldTick:ClearAllPoints()
 		if hasMissingHealth then
 			overshieldTick:SetPoint("TOPLEFT", healthBar, "TOPRIGHT", OVERSHIELD_TICK_OFFSET, 0)
 			overshieldTick:SetPoint("BOTTOMLEFT", healthBar, "BOTTOMRIGHT", OVERSHIELD_TICK_OFFSET, 0)
@@ -92,7 +93,8 @@ ns.HandleCompactUnitFrame_Update = function(frame)
 
 
 	-- Handle shieldOverlay visibility and positioning
-	shieldOverlay:ClearAllPoints()
+	clearAllPoints(shieldOverlay)
+	--shieldOverlay:ClearAllPoints()
 	shieldOverlay:SetParent(healthBar)
 	-- Apply the texture and ensure proper tiling
 	local tileSize = shieldOverlay.tileSize or 128
@@ -122,6 +124,7 @@ ns.HandleCompactUnitFrame_Update = function(frame)
 	end
 
 	-- Handle shieldBar visibility and positioning
+	clearAllPoints(shieldBar)
 	shieldBar:ClearAllPoints()
 	-- Apply custom color/alpha, blend mode, and texture to shieldBar
 	local shieldBarColor = db.shieldBarColor
