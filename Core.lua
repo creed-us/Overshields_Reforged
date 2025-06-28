aa20fb48c03849f91ecc23da591c6816c3eb3ce9
Creed <creedonhood@gmail.com>
2025-06-28 15:19:02 -0400

    formatting

--- Core.lua
+++ Core.lua
@@ -3,16 +3,16 @@ local AceAddon = LibStub("AceAddon-3.0")
 OvershieldsReforged = AceAddon:NewAddon(ADDON_NAME, "AceEvent-3.0", "AceConsole-3.0")
 
 function OvershieldsReforged:OnInitialize()
-	-- Initialize database and options
-	self:InitializeDatabase()
-	self:SetupOptions()
-	-- Hook Blizzard's heal-prediction functions
-	hooksecurefunc("UnitFrameHealPredictionBars_Update", function(frame)
-		ns.HandleUnitFrameUpdate(frame)
-	end)
-	hooksecurefunc("CompactUnitFrame_UpdateHealPrediction", function(frame)
-		ns.QueueCompactUnitFrameUpdate(frame)
-	end)
+    -- Initialize database and options
+    self:InitializeDatabase()
+    self:SetupOptions()
+    -- Hook Blizzard's heal-prediction functions
+    hooksecurefunc("UnitFrameHealPredictionBars_Update", function(frame)
+        ns.HandleUnitFrameUpdate(frame)
+    end)
+    hooksecurefunc("CompactUnitFrame_UpdateHealPrediction", function(frame)
+        ns.QueueCompactUnitFrameUpdate(frame)
+    end)
 end
 
 -- Slash command handler
