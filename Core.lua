0ee74be4420a546cc66aa5e2a3d12e8ce776840f
Creed <creedonhood@gmail.com>
2026-03-08 15:46:21 -0600

    rm string_sub def

--- Core.lua
+++ Core.lua
@@ -2,8 +2,6 @@ local ADDON_NAME, ns = ...
 local AceAddon = LibStub("AceAddon-3.0")
 OvershieldsReforged = AceAddon:NewAddon(ADDON_NAME, "AceEvent-3.0", "AceConsole-3.0")
 
-local string_sub = string.sub
-
 function OvershieldsReforged:OnInitialize()
 	self:InitializeDatabase()
 	self:SetupOptions()
