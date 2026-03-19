116c468ed1ca8eb1aabe27903bbef7a382bbbda6
Creed <creedonhood@gmail.com>
2026-03-19 09:20:10 -0600

    create Constants.lua, migrate frequent calls

---
+++ Constants.lua
@@ -0,0 +1,23 @@
+local _, ns = ...
+
+local string_find = string.find
+local CreateFrame = CreateFrame
+local UnitExists = UnitExists
+local UnitGetTotalAbsorbs = UnitGetTotalAbsorbs
+local GetTime = GetTime
+local wipe = wipe
+local next = next
+local pairs = pairs
+local ipairs = ipairs
+local IsInRaid = IsInRaid
+
+ns.string_find = string_find
+ns.CreateFrame = CreateFrame
+ns.UnitExists = UnitExists
+ns.UnitGetTotalAbsorbs = UnitGetTotalAbsorbs
+ns.GetTime = GetTime
+ns.wipe = wipe
+ns.next = next
+ns.pairs = pairs
+ns.ipairs = ipairs
+ns.IsInRaid = IsInRaid
